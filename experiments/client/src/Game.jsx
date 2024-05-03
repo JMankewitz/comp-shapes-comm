ff3cd5bdf56cc4615577c317c2c014d8852a379d
Jessica Mankewitz <jmankewitz@berkeley.edu>
2024-05-03 10:56:46 -0500

    first base tangram experiment

---
+++ experiments/client/src/Game.jsx
@@ -0,0 +1,36 @@
+import {Chat, useGame, useRound } from "@empirica/core/player/classic/react";
+
+import React from "react";
+import { Profile } from "./Profile";
+import { Task } from "./Task";
+
+// const roundSound = new Audio("experiment/round-sound.mp3");
+// const gameSound = new Audio("experiment/bell.mp3");
+
+export function Game() {
+  const game = useGame();
+  const round = useRound();
+  // Make some noise
+  // if (game.get("justStarted")) {
+  //   gameSound.play();
+  //   game.set("justStarted", false);
+  // } else {
+  //   roundSound.play();
+  // }
+
+  return (
+    <div className="h-full w-full flex">
+      <div className="h-full w-full flex flex-col">
+        <Profile />
+        <div className="h-full flex items-center justify-center">
+          <Task />
+        </div>
+      </div>
+
+      <div className="h-full w-128 border-l flex justify-center items-center">
+        <Chat scope={round} attribute="chat" 
+        />
+      </div>
+    </div>
+  );
+}
