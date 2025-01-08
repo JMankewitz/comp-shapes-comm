5fdc988b35c087e30ae6536d6e473b3f387bb6b9
Jess Mankewitz <jmankewitz@gmail.com>
2025-01-08 10:52:12 -0800

    add sentry

--- experiments/compositional-tangrams/client/src/index.jsx
+++ experiments/compositional-tangrams/client/src/index.jsx
@@ -6,6 +6,25 @@ import "../node_modules/@empirica/core/dist/player.css";
 import App from "./App";
 import "./index.css";
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://39dffa4a7bdcafb5290401881940b4fa@o4506525893853184.ingest.us.sentry.io/4508609472954368",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
