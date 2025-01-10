32aa49ce5d78fbf2934c63867085651f0859b089
jmankewitz <jmankewitz@gmail.com>
2025-01-10 21:28:37 +0000

    tweak condition timeout

--- experiments/compositional-tangrams/client/src/index.jsx
+++ experiments/compositional-tangrams/client/src/index.jsx
@@ -9,7 +9,7 @@ import "./index.css";
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-    replaysSessionSampleRate: 1.0,
+    replaysSessionSampleRate: 0.75,
     replaysOnErrorSampleRate: 1.0,
     dsn: "https://39dffa4a7bdcafb5290401881940b4fa@o4506525893853184.ingest.us.sentry.io/4508609472954368",
     integrations: [
@@ -21,7 +21,7 @@ Sentry.init({
         networkDetailHasUrls: true,
 	}),
     ],
-    release: "comp-shapes-comm@0.0.3",
+    release: "comp-shapes-comm@0.0.4",
 });
 
 
